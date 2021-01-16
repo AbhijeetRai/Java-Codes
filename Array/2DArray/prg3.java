b50b536652df239d507761ee9ef52bdfa5e46e35
AbhijeetRai <abraj306@gmail.com>
2021-01-16 19:51:00 +0530

    Jan 16

--- Array/2DArray/prg3.java
+++ Array/2DArray/prg3.java
@@ -5,7 +5,7 @@ class Demo {
                 int[][] arr1 = {new int[]{1, 2}, new int[]{3, 4}};
 
                 int[][] arr2 = {new int[2], new int[2]};
-
+                
                 int[][] arr3 = new int[2][2];   
                 arr3[0] = new int[2];
                 arr3[1] = new int[2];
